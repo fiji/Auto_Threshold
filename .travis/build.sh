32c4fac9176c90e08842877034bbb19a258ec1be
Curtis Rueden <ctrueden@wisc.edu>
2018-04-09 13:31:57 -0500

    Update Travis configuration
    Thanks, travisify.sh!

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/master/travis-build.sh
-sh travis-build.sh
+sh travis-build.sh $encrypted_f7ff3ad6a129_key $encrypted_f7ff3ad6a129_iv
---
+++ README.md
@@ -0,0 +1,2 @@
+[![](https://travis-ci.org/fiji/Auto_Threshold.svg?branch=master)](https://travis-ci.org/fiji/Auto_Threshold)
+
